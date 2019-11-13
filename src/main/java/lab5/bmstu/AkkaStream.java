4d49689e418a479b6acce89f73cdb618c21afdfa
x1Dman <f_ury102@mail.ru>
2019-11-13 13:02:50 +0300

    Scripted auto-commit on change (2019-11-13 13:02:50) by gitwatch.sh

--- src/main/java/lab5/bmstu/AkkaStream.java
+++ src/main/java/lab5/bmstu/AkkaStream.java
@@ -20,7 +20,8 @@ public class AkkaStream {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        //<вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = null;
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
