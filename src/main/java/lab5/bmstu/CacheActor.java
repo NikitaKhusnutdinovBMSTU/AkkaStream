5ec8a165f7930c5f7e30db8cf7e6fae8085df8f7
x1Dman <f_ury102@mail.ru>
2019-11-14 21:57:16 +0300

    Scripted auto-commit on change (2019-11-14 21:57:15) by gitwatch.sh

--- src/main/java/lab5/bmstu/CacheActor.java
+++ src/main/java/lab5/bmstu/CacheActor.java
@@ -1,7 +1,10 @@
 package lab5.bmstu;
 
 import akka.actor.AbstractActor;
+import akka.actor.Actor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
+import javafx.util.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,9 +16,20 @@ public class CacheActor extends AbstractActor {
     @Override
     public Receive createReceive() {
 
-        return ReceiveBuilder.create().match(GetMSG.class,
-                req -> {
-
-                }).build();
+        return ReceiveBuilder.create()
+                .match(GetMSG.class,
+                        msg -> {
+                            String url = msg.getURL();
+                            int count = msg.getCount();
+                            if (data.containsKey(url) && data.get(url).containsKey(count)) {
+                                getSender().tell(data.get(url).get(count), ActorRef.noSender());
+                            } else {
+                                getSender().tell(-1, ActorRef.noSender());
+                            }
+                        })
+                .match(PutMSG.class, msg -> {
+                    data.put();
+                })
+                .build();
     }
 }
--- src/main/java/lab5/bmstu/PutMSG.java
+++ src/main/java/lab5/bmstu/PutMSG.java
@@ -1,4 +1,23 @@
 package lab5.bmstu;
 
+import javafx.util.Pair;
+
 public class PutMSG {
+    private Pair<String, Pair<Integer, Integer>> msg;
+
+    public PutMSG(Pair<String, Pair<Integer, Integer>> msg){
+        this.msg = msg;
+    }
+
+    public String getURL(){
+        return msg.getKey();
+    }
+
+    public int getCount(){
+        return msg.getValue().getKey();
+    }
+
+    public int getTime(){
+        return msg.getValue().getValue();
+    }
 }
