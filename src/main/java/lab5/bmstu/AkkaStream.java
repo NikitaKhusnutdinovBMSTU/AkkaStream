482d413bb6ee3a1a120ca177bc01b5b92b033d7a
x1Dman <f_ury102@mail.ru>
2019-11-14 20:05:44 +0300

    Scripted auto-commit on change (2019-11-14 20:05:44) by gitwatch.sh

--- src/main/java/lab5/bmstu/AkkaStream.java
+++ src/main/java/lab5/bmstu/AkkaStream.java
@@ -94,7 +94,7 @@ public class AkkaStream {
                                 CompletionStage<HttpResponse> result = source.via(testSink).toMat(Sink.last(), Keep.right()).run(materializer);
                                 return result.toCompletableFuture().get();
                             } catch (Exception e) {
-                                return HttpResponse.create().withEntity(ByteString.fromString("exception is " + e.toString()));
+                                return HttpResponse.create().withEntity(ByteString.fromString("exception is "));
                             }
 
                         }
