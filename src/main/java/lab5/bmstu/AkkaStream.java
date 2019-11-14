f5b30acdee6ad017e5131f31dbfc375094e6b881
x1Dman <f_ury102@mail.ru>
2019-11-14 18:40:42 +0300

    Scripted auto-commit on change (2019-11-14 18:40:42) by gitwatch.sh

--- src/main/java/lab5/bmstu/AkkaStream.java
+++ src/main/java/lab5/bmstu/AkkaStream.java
@@ -15,6 +15,7 @@ import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import akka.util.ByteString;
 import akka.japi.Pair;
+import com.sun.tools.corba.se.idl.constExpr.Not;
 import scala.concurrent.Future;
 import scala.util.Try;
 
@@ -37,42 +38,54 @@ public class AkkaStream {
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.of(HttpRequest.class).map(
                 req -> {
 
-                    if(req.method() == HttpMethods.GET) {
+                    if (req.method() == HttpMethods.GET) {
                         if (req.getUri().path().equals("/")) {
                             String url = req.getUri().query().get("testUrl").orElse("");
                             String count = req.getUri().query().get("count").orElse("");
-                            if(url.isEmpty()){
+                            if (url.isEmpty()) {
                                 return HttpResponse.create().withEntity(ByteString.fromString("TEST URL IS EMPTY"));
                             }
-                            if(url.isEmpty()){
+                            if (url.isEmpty()) {
                                 return HttpResponse.create().withEntity(ByteString.fromString("COUNT IS EMPTY"));
                             }
-                            try{
+                            try {
                                 Integer countInteger = Integer.parseInt(count);
                                 Pair<String, Integer> data = new Pair<>(url, countInteger);
-
+                                Source<Pair<String, Integer>, NotUsed> source = Source.from(Collections.singletonList(data));
 
                                 Flow<Pair<String, Integer>, HttpResponse, NotUsed> testSink = Flow
                                         .<Pair<String, Integer>>create()
                                         .map(pair -> new Pair<>(HttpRequest.create().withUri((pair.first())), pair.second()))
-                                        .mapAsync(1, pair ->{
+                                        .mapAsync(1, pair -> {
                                             Flow<Pair<HttpRequest, Long>, Pair<Try<HttpResponse>, Long>, NotUsed> httpClient = http.superPool(materializer);
-                                            Sink<Pair<Try<HtppResponse>, Long>, CompletionStage<Integer>> fold = Sink.fold(0, (ac, el) -> {
-                                                
-                                            })
-                                        });
+                                            Sink<Pair<Try<HttpResponse>, Long>, CompletionStage<Integer>> fold = Sink.fold(0, (ac, el) -> {
+                                                int responseTime = (int) (System.currentTimeMillis() - el.second());
+                                                return ac + responseTime;
+                                            });
+                                            return Source.from(Collections.singletonList(pair))
+                                                    .toMat(
+                                                            Flow.<Pair<HttpRequest, Integer>>create()
+                                                                    .mapConcat(p -> Collections.nCopies(p.second(), p.first()))
+                                                                    .map(req2 -> new Pair<>(req2, System.currentTimeMillis())).via(httpClient).toMat(fold, Keep.right()), Keep.right()).run(materializer);
+                                        }).map(
+                                                sum -> {
+                                                    Double middleValue = (double) sum/ (double) countInteger;
+                                                    return HttpResponse.create().withEntity(ByteString.fromString("response is " + middleValue.toString()))
+                                                }
+                                        );
 
-
-                            } catch(Exception e){
+                                CompletionStage<HttpResponse> result = source.via(testSink).toMat(Sink.last(), Keep.right()).run(materializer);
+                                return result.toCompletableFuture().get();
+                            } catch (Exception e) {
                                 return HttpResponse.create().withEntity(ByteString.fromString("Some exception happened"));
                             }
 
                             return HttpResponse.create().withEntity(ContentTypes.TEXT_HTML_UTF8, ByteString.fromString("<html><body>Hello world!</body></html>"));
 
                         }
-                    }else{
-                            req.discardEntityBytes(materializer);
-                            return HttpResponse.create().withStatus(StatusCodes.NOT_FOUND).withEntity("NOPE");
+                    } else {
+                        req.discardEntityBytes(materializer);
+                        return HttpResponse.create().withStatus(StatusCodes.NOT_FOUND).withEntity("NOPE");
                     }
                 });
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
